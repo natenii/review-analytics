1c98015dc5f841dadf6dec52b28b22892d8a6d08
Nate Ni <natengai.nn@gmail.com>
2021-02-15 10:12:29 +0800

    Added sreaching good in comments.

--- read.py
+++ read.py
@@ -10,11 +10,16 @@ with open('reviews.txt', 'r') as f:
 print('檔案讀取完畢，總共有', len(data), '筆資料')
 
 new = []
-#num = 0
 for d in data:
     if len(d) < 100:
-        #num += 1
         new.append(d)
 print(len(new))
 print(new[0])
-#print('留言少於100字的有', num, '筆資料')
+print('留言少於100字的有', len(new), '筆資料')
+
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print('留言裡有good的有', len(good), '筆資料')
+print(good[0])
