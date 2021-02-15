6f55a3c6c891dbb41bb0e0bbe7c989045639b4cd
Nate Ni <natengai.nn@gmail.com>
2021-02-15 09:54:22 +0800

    Added filter function

--- read.py
+++ read.py
@@ -1,16 +1,20 @@
 data = []
-count = 0
+# count = 0
 
 with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
-        count += 1
-        if count % 1000 == 0:
-            print(len(data))
+        # count += 1
+        # if count % 1000 == 0:
+        #     print(len(data))
 print('檔案讀取完畢，總共有', len(data), '筆資料')
 
-sum_len = 0
+new = []
+#num = 0
 for d in data:
-    sum_len += len(d)
-
-print('平均留言長度為' ,sum_len/len(data)) 
+    if len(d) < 100:
+        #num += 1
+        new.append(d)
+print(len(new))
+print(new[0])
+#print('留言少於100字的有', num, '筆資料')
