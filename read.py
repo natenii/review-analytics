24ef3952d1e23ac0f8c0d9c98b3bc95b001bbb44
Nate Ni <natengai.nn@gmail.com>
2021-02-23 13:17:51 +0800

    add dictionary for word count and searching

--- read.py
+++ read.py
@@ -9,27 +9,58 @@ with open('reviews.txt', 'r') as f:
         #     print(len(data))
 print('檔案讀取完畢，總共有', len(data), '筆資料')
 
-new = []
+wc = {} #word count
 for d in data:
-    if len(d) < 100:
-        new.append(d)
-print(len(new))
-print(new[0])
-print('留言少於100字的有', len(new), '筆資料')
+    words = d.split()
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1
 
-good = []
-for d in data:
-    if 'good' in d:
-        good.append(d)
-print('留言裡有good的有', len(good), '筆資料')
-print(good[0])
+for word in wc:
+    if wc[word] >100000:
+        print(word, wc[word])
 
-good = [d for d in data if 'good' in d] #The first d equals to good.append(d)
-print(good)
+#print(len(wc))
 
-bad = ['bad' in d for d in data]
-print(bad)
+while True:
+    word = input('請問你想找什麼字： ')
+    if word == 'q':
+        break
+    if word in wc:
+        print(word, '出現過', wc[word], '次')
+    else:
+        print('這個字沒有出現過！')
 
-bad = []
-for d in data:
-    bad.append('bad' in d)
+print('感謝使用！')
+
+# sum_len = 0
+# for d in data:
+#     sum_len += len(d)
+# print('留言的平均長度為',  sum_len/len(data))
+
+# new = []
+# for d in data:
+#     if len(d) < 100:
+#         new.append(d)
+# print(len(new))
+# print(new[0])
+# print('留言少於100字的有', len(new), '筆資料')
+
+# good = []
+# for d in data:
+#     if 'good' in d:
+#         good.append(d)
+# print('留言裡有good的有', len(good), '筆資料')
+# print(good[0])
+
+# good = [d for d in data if 'good' in d] #The first d equals to good.append(d)
+# print(good)
+
+# bad = ['bad' in d for d in data]
+# print(bad)
+
+# bad = []
+# for d in data:
+#     bad.append('bad' in d)
